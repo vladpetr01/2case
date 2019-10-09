d91d66f1e6f5809cd61c259c8aa32da99660b89f
Mauter25 <mauters@mail.ru>
2019-10-09 16:38:15 +0700

    done.

--- Progressive taxation.py
+++ Progressive taxation.py
@@ -1,3 +1,4 @@
+type_family = input('Выберите тип семьи:\n1.один 2.пара 3. одиночка')
 JAN = 'Январе'
 FAB = 'Феврале'
 MAR = 'Марте'
@@ -16,3 +17,11 @@ for month in range(12):
     print('{} {}:'.format('СКОЛЬКО ТЫ ЗАРАБОТАЛ В' , name_month[month], end = ''))
     income = float(input())
     annual_income += income
+if type_family == '1':
+    if 0 < annual_income <= 9075:
+        tax = 0.1 * annual_income
+    elif 9076 <= annual_income <= 36900:
+        tax = 0.1 * 9075 + 0.15 * (annual_income - 9075)
+    elif 36901 <= annual_income <= 89350:
+        tax = 0.1 * 9075 + 0.15 * (36900 - 9075) + 0.25 * (annual_income - 36900)
+print(tax)
